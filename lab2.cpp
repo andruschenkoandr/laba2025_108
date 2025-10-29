8097c58cc1f9e429680cd7f788676dd5cc14a6b3
Andruschenko Andrew <andruschenkoandr@gmail.com>
2025-10-29 11:42:38 +0200

    Оновив код

--- lab2.cpp
+++ lab2.cpp
@@ -6,7 +6,7 @@ int main()
 	cout << dec << showpos << left << '\t' << setw(3) << 4 << "\n" << endl;
 	cout << fixed << setprecision(2) << setw(15) << internal << -67.09124 << "\n" << endl;
 	cout << showpos << right << setw(10) << 235 << "\n" << endl;
-	cout << hex << 2209 << "\n" << dec << endl;
+	cout << hex << 2309 << "\n" << dec << endl;
 	cout << fixed << setprecision(2) << -121.0 << "\n" << endl;
 	cout << showpos << dec << 1 << 24 << "\n" << endl;
 }
